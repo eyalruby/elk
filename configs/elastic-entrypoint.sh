48a7a4f2187b972d65f99c2926f9e770199d5199
eyalruby <eyalruby@gmail.com>
2025-01-24 22:55:42 +0200

    Added s3 SSL certs

--- configs/elastic-entrypoint.sh
+++ configs/elastic-entrypoint.sh
@@ -11,6 +11,18 @@ echo "Adding S3 credentials to keystore..."
 echo $S3_ACCESS_KEY | bin/elasticsearch-keystore add --stdin s3.client.default.access_key
 echo $S3_SECRET_KEY | bin/elasticsearch-keystore add --stdin s3.client.default.secret_key
 
+# Import SSL certificate for S3 endpoint
+if [ ! -f /usr/share/elasticsearch/config/certs/public.crt ]; then
+    echo "Fetching SSL certificate for S3 endpoint..."
+    openssl s_client -connect $S3_ENDPOINT:443 < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /usr/share/elasticsearch/config/certs/public.crt
+
+    echo "Importing SSL certificate into Elasticsearch keystore..."
+    /usr/share/elasticsearch/jdk/bin/keytool -import -trustcacerts \
+        -alias s3-cert \
+        -keystore /usr/share/elasticsearch/config/certs/s3-truststore.jks \
+        -file /usr/share/elasticsearch/config/certs/public.crt -storepass changeit -noprompt
+fi
+
 # Set correct permissions
 chown elasticsearch:root config/elasticsearch.keystore
 
